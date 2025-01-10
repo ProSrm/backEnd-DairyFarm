7617cf6e54ac57067b5cb71b5417297913f480db
Prasad Kailasrao Jadhav <103479103+PrasadKailasraoJadhav@users.noreply.github.com>
2025-01-10 18:22:31 +0530

    Added  login Controller (#2)
    * Added Login controller
    
    * Changes
    
    ---------
    
    Co-authored-by: PrasadJadhav <prasad.k.jadhav@softmass.co.in>

---
+++ src/dairyFarm/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using dairyFarm.DbContexts;
+using dairyFarm.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dairyFarm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DFdbContext _context;
+
+        public LoginController(DFdbContext context)
+        {
+            _context = context;
+        }       
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] Login login)
+        {
+            if (login == null || string.IsNullOrEmpty(login.Email) || string.IsNullOrEmpty(login.Password))
+            {
+                return BadRequest(new { message = "Email and Password are required." });
+            }
+
+            var user = await _context.Login
+                .FirstOrDefaultAsync(u => u.Email == login.Email && u.Password == login.Password);
+
+            if (user == null)
+            {
+                return Unauthorized(new { message = "Invalid email or password." });
+            }
+
+            return Ok(new
+            {
+                email = user.Email,
+                role = user.Authority,
+                message = "Login successful"
+            });
+        }
+    }
+}
---
+++ src/dairyFarm/TypeConfig/LoginEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using dairyFarm.Entity;
+
+namespace dairyFarm.TypeConfig
+{
+    public class LoginEntityTypeConfiguration : IEntityTypeConfiguration<Login>
+    {
+        private readonly string _tableName;
+        private readonly string _schemaName;
+
+        public LoginEntityTypeConfiguration(string tableName, string schemaName)
+        {
+            _tableName = tableName;
+            _schemaName = schemaName;
+        }
+        public void Configure(EntityTypeBuilder<Login> builder)
+        {
+            builder.ToTable(_tableName, _schemaName);
+            builder.Property(k => k.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
